f216af488418d52fbc09eb2490dc86d67479f2f1
jhenifer <jhenifermachado211@gmail.com>
2022-05-30 11:16:54 -0300

    master

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <header>
+    <span class="texto-header">Home</span>
+    <span class="separador">|</span>
+    <span class="texto-header">Jogadores</span>
+    <span class="separador">|</span>
+    <span class="texto-header">Times</span>
+    <span class="separador">|</span>
+    
+    </header>
+  <main>Main</main>
+  <footer>Rodapé</footer>
+</template>
+
+<style>
+@import "@/assets/base.css";
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  height: 100vh;
+  /* padding: 2rem; */
+
+  font-weight: normal;
+}
+
+header,
+footer {
+  height: 10%;
+  background-color: #db7093;
+  color: black;
+  font-size: 1.3rem;
+  display: flex;
+  align-items: center;
+}
+
+main {
+  height: 80%;
+  background-color: crimson;
+}
+</style>
